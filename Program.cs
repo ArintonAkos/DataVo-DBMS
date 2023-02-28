98a47370e35cedab5a4f85f7d2625283b87e9786
ArintonAkos <43067524+ArintonAkos@users.noreply.github.com>
2023-02-28 19:28:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ABKR.Parser;
+using ABKR.Utils;
+
+try
+{
+    string inputFilePath = ConsoleInputHandler.GetSourceFileName();
+    Parser parser = new(inputFileName);
+
+    Console.WriteLine(parser.Parse(FileHandler.GetFileText(inputFileName)));
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
+
+String handleConsoleInput()
+{
+
+}
