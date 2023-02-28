f5c0435dd4b3cf8d37d97cc924134aa1649a5c79
ArintonAkos <43067524+ArintonAkos@users.noreply.github.com>
2023-02-28 19:28:32 +0200

    Add project files.

---
+++ Parser/Parser.cs
@@ -0,0 +1,23 @@
+﻿using ABKR.Utils;
+using System;
+
+
+namespace ABKR.Parser
+{
+    public class Parser
+    {
+        private String _filePath { get; set; }
+
+        public Parser(String filePath)
+        {
+            this._filePath = filePath;
+        }
+
+        public String Parse()
+        {
+            String fileContent = FileHandler.GetFileText(this._filePath);
+
+            return String.Empty;
+        }
+    }
+}
---
+++ Parser/ParserConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABKR.Parser
+{
+    internal class ParserConfig
+    {
+        public static String OUTPUT_FILE_NAME = "";
+
+        public static Boolean IS_TEST = false;
+
+        public static String FILE_EXTENSION = "abkr";
+    }
+}
