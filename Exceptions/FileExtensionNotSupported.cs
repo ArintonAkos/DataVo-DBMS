8964f23825c2acb27af1e8ceb1572d7f6631b540
ArintonAkos <43067524+ArintonAkos@users.noreply.github.com>
2023-02-28 20:59:41 +0200

    Add project files.

---
+++ Exceptions/FileExtensionNotSupported.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABKR.Exceptions
+{
+    internal class FileExtensionNotSupported : Exception
+    {
+        public FileExtensionNotSupported(string extension) 
+            : base($"File extension not supported! Only *.{extension} files can be compiled!")
+        {
+        }
+    }
+}
---
+++ Exceptions/NoSourceFileProvided.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABKR.Exceptions
+{
+    internal class NoSourceFileProvided : Exception
+    {
+        public NoSourceFileProvided()
+            : base("No source file was found when calling the compiler!")
+        { }
+    }
+}
