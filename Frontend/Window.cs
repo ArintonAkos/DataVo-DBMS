7373be871e75b07aa10f61d77ae7db8c657ada6b
Bulcsú Veres <bulcsu.veres@gmail.com>
2023-03-07 23:51:28 +0200

    Frontend basics

---
+++ Frontend/Window.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Frontend
+{
+    public partial class Window : Form
+    {
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stripExecute_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuNewFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuOpenFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuExit_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
