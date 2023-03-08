9e37614638c2174f2c8d57969e44ef705dd327f3
Bulcsú Veres <bulcsu.veres@gmail.com>
2023-03-08 22:21:55 +0200

    Updated functionality

--- Frontend/Window.cs
+++ Frontend/Window.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Schema;
 
 namespace Frontend
 {
@@ -19,7 +22,7 @@ namespace Frontend
 
         private void Window_Load(object sender, EventArgs e)
         {
-
+            stripFilenameLabel.Text = string.Empty;
         }
 
         private void stripExecute_Click(object sender, EventArgs e)
@@ -29,17 +32,50 @@ namespace Frontend
 
         private void menuNewFile_Click(object sender, EventArgs e)
         {
+            SaveFileDialog fileChooser = new SaveFileDialog();
 
+            fileChooser.InitialDirectory = "c:\\";
+            fileChooser.Filter = "SQL files (*.sql)|*.sql";
+            fileChooser.RestoreDirectory = true;
+
+            if (fileChooser.ShowDialog() == DialogResult.OK)
+            {
+                stripFilenameLabel.Text = fileChooser.FileName;
+                File.Create(stripFilenameLabel.Text);
+            }
         }
 
         private void menuOpenFile_Click(object sender, EventArgs e)
         {
+            OpenFileDialog fileChooser = new OpenFileDialog();
 
+            fileChooser.InitialDirectory = "c:\\";
+            fileChooser.Filter = "SQL files (*.sql)|*.sql";
+            fileChooser.RestoreDirectory = true;
+
+            if (fileChooser.ShowDialog() == DialogResult.OK)
+            {
+                stripFilenameLabel.Text = fileChooser.FileName;
+                textEditor.Text = File.ReadAllText(stripFilenameLabel.Text);
+            }
+        }
+
+        private void menuSaveFile_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(stripFilenameLabel.Text, textEditor.Text);
         }
 
         private void menuExit_Click(object sender, EventArgs e)
         {
+            Application.Exit();
+        }
 
+        private void textEditor_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Modifiers == Keys.Control && e.KeyCode == Keys.S)
+            {
+                menuSaveFile_Click(this, e);
+            }
         }
     }
 }
