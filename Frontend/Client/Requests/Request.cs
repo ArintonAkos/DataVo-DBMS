84c11e3819daafbc7b412682b0dcb9a2cd4b6c4b
Bulcsú Veres <bulcsu.veres@gmail.com>
2023-03-08 20:55:26 +0200

    Working frontend

---
+++ Frontend/Client/Requests/Request.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Frontend.Client.Requests
+{
+    internal class Request
+    {
+        [JsonProperty("command_type")]
+        public String CommandType { get; set; }
+        [JsonProperty("data")]
+        public String Data { get; set; }
+    }
+}
