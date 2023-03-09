14b0f8a1f19017a1e8ab4c249c38459286f725df
Bulcsú Veres <bulcsu.veres@gmail.com>
2023-03-09 19:40:21 +0200

    Updated request handling

--- Frontend/Client/Requests/Request.cs
+++ Frontend/Client/Requests/Request.cs
@@ -5,8 +5,6 @@ namespace Frontend.Client.Requests
 {
     internal class Request
     {
-        [JsonProperty("command_type")]
-        public String CommandType { get; set; }
         [JsonProperty("data")]
         public String Data { get; set; }
     }
