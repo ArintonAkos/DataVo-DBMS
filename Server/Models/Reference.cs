962e0a3b5cd8e06d79ba7a2eb7ea3fc1b4ab7dfd
Bulcsú Veres <bulcsu.veres@gmail.com>
2023-03-11 02:56:53 +0200

    Refactored database and table models

---
+++ Server/Models/Reference.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Server.Models
+{
+    [Serializable]
+    [XmlRoot("References")]
+    public class Reference
+    {
+        [XmlElement("RefTable")]
+        public String ReferenceTableName { get; set; }
+        
+        [XmlElement("RefAttribute")]
+        public String ReferenceAttributeName { get; set; }
+    }
+}
---
+++ Server/Models/Table.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Server.Models
+{
+    public class Table
+    {
+        public List<Field> Fields { get; set; }
+        
+        [XmlArray("PrimaryKeys")]
+        [XmlArrayItem("PkAttribute")]
+        public List<string> PrimaryKeys { get; set; }
+
+        [XmlArray("ForeignKeys")]
+        [XmlArrayItem("ForeignKey")]
+        public List<ForeignKey> ForeignKeys { get; set; }
+    }
+}
