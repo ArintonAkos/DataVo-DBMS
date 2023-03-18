df05f731d592ea6f9274abbbcf6bf8cf8081e6d6
ArintonAkos <arintonakos@gmail.com>
2023-03-18 13:59:25 +0200

    Updated Data data strucutre (used for stroring the database data) and created a factory for it for making the use more obvious

---
+++ Server/Models/DML/InsertIntoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Models.DML
+{
+    internal class InsertIntoModel
+    {
+        public String DatabaseName { get; set; }
+        public List<Dictionary<String, Data>> Values { get; set; }
+    }
+}
