d7f00108b9b775e72066dbfe8ba8959951e9fa7d
ArintonAkos <arintonakos@gmail.com>
2023-06-02 01:27:52 +0300

    Created Aggregations

---
+++ Server/Parser/Aggregations/Max.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Parser.Aggregations
+{
+    internal class Max : Aggregation
+    {
+        public Max(string field) : base(field) { }
+
+        public override dynamic Apply(List<Dictionary<string, dynamic>> rows)
+        {
+            return rows.Max(row => row[_field]);
+        }
+    }
+
+}
---
+++ Server/Parser/Aggregations/Sum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Parser.Aggregations
+{
+    internal class Sum : Aggregation
+    {
+        public Sum(string field) : base(field) { }
+
+        public override dynamic Apply(List<Dictionary<string, dynamic>> rows)
+        {
+            return rows.Sum(row => (double)row[_field]);
+        }
+    }
+}
