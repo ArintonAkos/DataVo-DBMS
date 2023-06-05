72217d0cac671fda9a77a5f70f27f65453b8e80a
ArintonAkos <arintonakos@gmail.com>
2023-06-06 00:49:54 +0300

    Refactored interfaces and created a new foldr for storing them

--- Server/Parser/Actions/IDbAction.cs
+++
@@ -1,9 +0,0 @@
-﻿using Server.Server.Requests.Controllers.Parser;
-using Server.Server.Responses;
-
-namespace Server.Parser.Actions;
-
-internal interface IDbAction
-{
-    public ActionResponse Perform(Guid session);
-}
