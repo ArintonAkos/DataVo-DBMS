68b0bc4ee6ebc236a2c8b9135a53f857b0ca0e51
ArintonAkos <arintonakos@gmail.com>
2023-06-06 00:50:25 +0300

    Created a new service for working with Columns

---
+++ Server/Services/TableColumnService.cs
@@ -0,0 +1,47 @@
+﻿using Server.Contracts;
+
+namespace Server.Services
+{
+    internal class TableColumnService
+    {
+        public static bool IsNumeric(IColumn column)
+        {
+            return IsInteger(column) || IsFloat(column);
+        }
+
+        public static bool IsInteger(IColumn column)
+        {
+            string type = column.RawType().ToLower();
+
+            return (type == "int");
+        }
+
+        public static bool IsFloat(IColumn column)
+        {
+            string type = column.RawType().ToLower();
+
+            return (type == "float");
+        }
+
+        public static bool IsBoolean(IColumn column)
+        {
+            string type = column.RawType().ToLower();
+
+            return (type == "bit");
+        }
+
+        public static bool IsString(IColumn column)
+        {
+            string type = column.RawType().ToLower();
+
+            return (type == "varchar");
+        }
+        
+        public static bool IsDate(IColumn column)
+        {
+            string type = column.RawType().ToLower();
+
+            return (type == "date");
+        }
+    }
+}
